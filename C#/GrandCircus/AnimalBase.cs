9a090fb58a8d815d52b5157b8d50dd6f0eef26e1
Cata-gg <catalin.gandila@gmail.com>
2026-04-05 20:00:36 +0300

    Homework-C# Done

---
+++ C#/GrandCircus/AnimalBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace RemoteLearning.GrandCircus
+{
+    internal class   AnimalBase : IAnimal
+    {
+
+      public string SpeciesName { get; set; }
+      public string Name {  get; set; }
+
+       public AnimalBase (string name, string speciesName)
+        {
+            Name = name;
+            SpeciesName = speciesName;
+        }
+        public  virtual string MakeSound()
+        {
+            return "Sound made by the animal: ";
+        }
+    }
+}
